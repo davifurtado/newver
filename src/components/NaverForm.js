f03e107f5c6bd7ad6a06083f23a35a7306ef4739
Davi Furtado <44845457+davifurtado@users.noreply.github.com>
2020-08-25 19:46:51 -0300

    fields to create a naver formated

--- src/components/NaverForm.js
+++ src/components/NaverForm.js
@@ -7,8 +7,8 @@ const NaverForm = ({ naver }) => {
         e.preventDefault();
         createNaver({
             name: e.target.name.value,
-            birthdate: e.target.birthdate.value,
-            admission_date: e.target.admission_date.value,
+            birthdate: `${e.target.birthdate.value.replace('-','/')} 00:00:00`,
+            admission_date: `${e.target.admission_date.value.replace('-','/')} 00:00:00`,
             job_role: e.target.job_role.value,
             project: e.target.project.value,
             url: e.target.url.value
@@ -18,27 +18,27 @@ const NaverForm = ({ naver }) => {
         <form>
             <div>
                 <label>Nome:</label>
-                <input value={naver.name} />
+                <input required minLength={3} value={naver.name} />
             </div>
             <div>
                 <label>Data Admissão: </label>
-                <input value={ naver.admission_date }/>
+                <input required  value={ naver.admission_date }/>
             </div>
             <div>
                 <label>Cargo: </label>
-                <input value={ naver.job_role } />
+                <input required minLength={3} value={ naver.job_role } />
             </div>
             <div>
                 <label>Projeto: </label>
-                <input value={ naver.project } />
+                <input required  value={ naver.project } />
             </div>
             <div>
                 <label>Aniversário: </label>
-                <input value={ naver.birthdate }/>
+                <input required  value={ naver.birthdate }/>
             </div>
             <div>
-                <label>URL: </label>
-                <input name="url"/>
+                <label >URL: </label>
+                <input required minLength={5} name="url"/>
             </div>
             <div>
                 <button type="submit">Gravar</button>
@@ -48,27 +48,27 @@ const NaverForm = ({ naver }) => {
         <form onSubmit={handleOnSubmit}>
             <div>
                 <label>Nome:</label>
-                <input name="name"/>
+                <input required minLength={3} name="name"/>
             </div>
             <div>
                 <label>Data Admissão: </label>
-                <input name="admission_date"/>
+                <input type="date" name="admission_date"/>
             </div>
             <div>
                 <label>Cargo: </label>
-                <input name="job_role"/>
+                <input required minLength={3} name="job_role"/>
             </div>
             <div>
                 <label>Projeto: </label>
-                <input name="project"/>
+                <input required name="project"/>
             </div>
             <div>
                 <label>Aniversário: </label>
-                <input name="birthdate"/>
+                <input type="date" name="birthdate"/>
             </div>
             <div>
                 <label>URL: </label>
-                <input name="url"/>
+                <input required minLength={5} name="url"/>
             </div>
             <div>
                 <button type="submit">Cadastrar</button>
