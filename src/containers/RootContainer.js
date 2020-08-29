87f73a9b02b490786e24f174d36e537a5e3f55c3
Davi Furtado <44845457+davifurtado@users.noreply.github.com>
2020-08-29 17:01:27 -0300

    adjusted global layout of the application

--- src/containers/RootContainer.js
+++ src/containers/RootContainer.js
@@ -8,16 +8,43 @@ import NaversList from '../components/NaversList';
 const RootContainer = () => {
     const { user } = useContext(AuthContext);
     return user.length ? (
-        <div>
+        <div
+            style={{ 
+                height: '100%',
+                padding: '10px',
+                backgroundColor: '#f5f6f7'
+            }}
+        >
             <NaverContextProvider>
                 <NavBar />
                 <NaversList />
             </NaverContextProvider>
         </div>
     ) : (
-        <div className="root-container">
-            <h1>Faça Login para prosseguir</h1>
-            <UserForm />
+        <div
+            style={{ 
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '100%'
+            }}
+        >
+            <div
+                style={{
+                    width: '50%',
+                    height: '50%',
+                    border: '2px solid',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    backgroundColor: '#f5f6f7'
+                }}
+            >
+                <h1>Faça Login para prosseguir</h1>
+                <UserForm />
+            </div>
         </div>
     )
 }
